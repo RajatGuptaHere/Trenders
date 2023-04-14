ebadd95276701ca17b804eedf00fb2bb475cd49e
RajatGuptaHere <rajatgupta3141@gmail.com>
2023-04-14 23:05:48 +0530

    first commit

--- db/connection.js
+++ db/connection.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 
-const DB = process.env.MONGODB_URI;
-mongoose.connect(DB,{
+const DB = process.env.DATABASE;
+mongoose.connect(DB, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 
-}).then(()=>{
+}).then(() => {
     console.log("connected to database")
-}).catch((err)=>{
+}).catch((err) => {
     console.log(err)
 })
